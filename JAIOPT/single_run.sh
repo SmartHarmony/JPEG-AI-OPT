3b7597bd71bbcd1861b14cbca7d14b0928bf0ca9
Zhenqing Hu <hustc12@gmail.com>
2023-11-07 14:12:20 -0500

    Updated the model path from the configure file.
    Signed-off-by: Zhenqing Hu <hustc12@gmail.com>

---
+++ JAIOPT/single_run.sh
@@ -0,0 +1,2 @@
+python ./lothar/controller.py convert --warmup_round=5 --config=/home/huzq85/2-working/JPEG-AI-OPT/Verification_Models/legacy/JPEG_AI_legacy.yml
+python ./lothar/controller.py run --warmup_round=5 --config=/home/huzq85/2-working/JPEG-AI-OPT/Verification_Models/legacy/JPEG_AI_legacy.yml
