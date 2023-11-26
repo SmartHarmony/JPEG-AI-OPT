6297f6bf02f00e1fcf41f618a6b6409e1c0bc48d
Zhenqing Hu <hustc12@gmail.com>
2023-11-26 17:03:29 -0500

    Minor updated the module of validate.
    Signed-off-by: Zhenqing Hu <hustc12@gmail.com>

--- JAIOPT/lothar/validate.py
+++ JAIOPT/lothar/validate.py
@@ -80,8 +80,9 @@ def calculate_similarity(u, v, data_type=np.float64):
 
 def calculate_pixel_accuracy(out_value, deepvan_out_value):
     if len(out_value.shape) < 2:
-        is_equal = lambda x, y: abs(x - y) < abs(x + y) * 1e-1
-        correct_count = sum(is_equal(x, y) for x, y in zip(out_value, deepvan_out_value))        
+        # is_equal = lambda x, y: abs(x - y) < abs(x + y) * 1e-1
+        is_equal = lambda x, y: abs(x - y) < 1e-4
+        correct_count = sum(is_equal(x, y) for x, y in zip(out_value, deepvan_out_value))
         return 1.0 * correct_count / out_value.shape[0]
     out_value = out_value.reshape((-1, out_value.shape[-1]))
     batches = out_value.shape[0]
@@ -93,18 +94,29 @@ def calculate_pixel_accuracy(out_value, deepvan_out_value):
             correct_count += 1
     return 1.0 * correct_count / batches
 
-
 def compare_output(platform, device_type, output_name, deepvan_out_value,
                    out_value, validation_threshold, log_file):
+    max_abs_diff = 0
+    max_abs_x = 0
+    max_abs_y=0
     if deepvan_out_value.size != 0:
         pixel_accuracy = calculate_pixel_accuracy(out_value, deepvan_out_value)
+        print(f'Pixel Accuracy = {pixel_accuracy}')
         if pixel_accuracy < 1 or len(out_value.shape) <= 2:
             # check more when pixel_accuracy is less than a threshold
             temp_out_value = list(out_value.flat)
             temp_deepvan_out_value = list(deepvan_out_value.flat)
             is_equal = lambda x, y: abs(x - y) < min(
-                abs(x) / 100, abs(y) / 100) or abs(x - y) < 5e-2
+                abs(x) / 100, abs(y) / 100) or abs(x - y) < 1e-4
             not_same = sum(not is_equal(x, y) for x, y in zip(temp_out_value, temp_deepvan_out_value))
+            for x, y in zip(temp_out_value, temp_deepvan_out_value):
+                if not is_equal(x, y) and abs(y-x) > max_abs_diff:
+                    max_abs_diff = abs(y-x)
+                    max_abs_x = x
+                    max_abs_y = y
+            if max_abs_diff != 0:
+                print(f'deepvan_out = {max_abs_y} != out = {max_abs_x},abs({max_abs_y}-{max_abs_x} = {max_abs_diff})')
+
             total_count = len(temp_deepvan_out_value)
             common.DLogger.summary(common.StringFormatter.block(
                 "pixel by pixel mismatch/total: %s/%s, mismatch rate: %s %%" % 
